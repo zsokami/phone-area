cdb1cdb9b0de6f75f180940eb3a0b9100f68423d
钟孟捷 <718335940@qq.com>
2024-09-11 02:26:49 +0800

    t

--- main.ts
+++ main.ts
@@ -1,7 +1,17 @@
-Deno.serve((req) => {
+import { acceptsLanguages } from 'jsr:@std/http/negotiation'
+
+let count = 0
+
+Deno.serve((req, { remoteAddr }) => {
   const url = new URL(req.url)
   if (url.pathname === '/') {
-    const html = Deno.readTextFileSync('index.html')
+    let html = Deno.readTextFileSync('index.html')
+    const toReplace: Record<string, string> = {
+      lang: acceptsLanguages(req, 'zh', 'en') ?? 'en',
+      count: `${++count}`,
+      remoteAddr: `${remoteAddr.hostname}:${remoteAddr.port}`,
+    }
+    html = html.replace(/{{(\w+)}}/g, (_, k) => toReplace[k] ?? k)
     return new Response(html, { headers: { 'content-type': 'text/html' } })
   }
   return new Response(null, { status: 404 })
